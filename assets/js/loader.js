1e295da044d6f8ea4527480c0e66b8dcebd04689
ze_tech <jmsj777@gmail.com>
2019-10-19 07:46:53 -0300

    com load pronto

---
+++ assets/js/loader.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function() {
+  setTimeout(() => {
+    const loader = document.querySelector(".loader");
+
+    loader.className += " hidden"; // class "loader hidden"
+  }, 500);
+});
